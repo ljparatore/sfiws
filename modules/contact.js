fdb366666ea12d89b436910553dbf7c5f951ce1f
ljparatore <ljparatore@gmail.com>
2016-09-04 22:06:30 -0400

    Update contact.js

--- modules/contact.js
+++ modules/contact.js
@@ -37,7 +37,7 @@ exports.execute = (req, res) => {
         })
         .catch(error => {
             if (error.code == 401) {
-                res.push(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
             } else {
                 res.send("An error as occurred");
             }
